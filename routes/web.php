89e3abb12f6d40fdd7ee7ec9c9056eebc42a1fb4
Shaneika Lewis <amberstudentneika@gmail.com>
2021-09-21 22:41:52 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//nm,mnmnkmmn
 Route::get('/', function () {
     return view('welcome');
 });
